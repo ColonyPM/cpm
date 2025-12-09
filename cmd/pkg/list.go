f079a6b6ab312aaff96ab9a9b1cb6f3aef928434
vidarnilsson <vidnil-3@student.ltu.se>
2025-12-09 18:17:15 +0100

    feat: pkg list cmd

--- cmd/pkg/list.go
+++ cmd/pkg/list.go
@@ -2,17 +2,37 @@ package pkgcmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/ColonyPM/cpm/internal/pkg"
 	"github.com/spf13/cobra"
 )
+
+func listPackages(cmd *cobra.Command, args []string) {
+	packagesPath, err := pkg.GetOrMakePackagesDirectory()
+	if err != nil {
+		fmt.Printf("Error getting packages directory: %v\n", err)
+		return
+	}
+
+	fmt.Println("Installed packages:")
+
+	entries, err := os.ReadDir(packagesPath)
+	if err != nil {
+		fmt.Printf("Error reading packages directory: %v\n", err)
+		return
+	}
+
+	for _, entry := range entries {
+		fmt.Println("    - " + entry.Name())
+	}
+}
 
 func newPkgListCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "list",
 		Aliases: []string{"ls"},
-		Short:   "List packages",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("pkg list called")
-		},
+		Short:   "List locally installedpackages",
+		Run:     listPackages,
 	}
 }
