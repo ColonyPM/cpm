5f0ef8ca2db1898d4b4c6acb438f8a35915ac702
david1br@gmail.com <david01br@gmail.com>
2025-12-10 21:30:00 +0100

    Feat: pkg remove cmd

--- cmd/pkg/remove.go
+++ cmd/pkg/remove.go
@@ -4,16 +4,59 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/ColonyPM/cpm/internal/pkg"
+	"path/filepath"
+	"os"
+	"strings"
+	//"errors"
 )
+ 
+var All bool
+func removepkg(cmd *cobra.Command, args []string) error {
+	//get packagedir
+	pkgsdir,err := pkg.GetOrMakePackagesDirectory()
+	if err != nil {	
+		return fmt.Errorf("error creating values.yaml: %w", err)
+	}
+	println(pkgsdir)
+	//Split name and version
+
+	pkgsname, version, versionexist:= strings.Cut(args[0],"@")
+	//If version is absent, fail for now
+
+
+	//Move to directory
+		pkgspath := filepath.Join(pkgsdir,pkgsname)
+		err = os.Chdir(pkgspath)
+		if err != nil {
+			return fmt.Errorf("No package found: %w", err)
+		}
+	if versionexist == true {
+		pkgsversionpath := filepath.Join(pkgspath,version)
+		err = os.RemoveAll(pkgsversionpath)
+	}
+	All, _:= cmd.Flags().GetBool("All")
+	if All == true {
+	err = os.RemoveAll(pkgspath)
+	}
+	
+	println("package",args[0],"removed")
+	
+	return(nil)
+}
 
 func newPkgRemoveCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:     "remove <pkg>",
 		Aliases: []string{"rm"},
 		Short:   "Remove a package",
 		Args:    cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("pkg remove called")
-		},
+		RunE: removepkg,
 	}
+	// Local flag: only applies to `serve`.
+	cmd.Flags().BoolVar(&All, "All", false, "remove all versions")
+
+	return cmd
 }
+
+
