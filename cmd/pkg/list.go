47642eeceeda3f9453ae4e4be126ad45bbfb34f1
Vincent <vincent.mick.gustafsson@gmail.com>
2026-01-28 10:31:03 +0100

    refactor: pkg list

--- cmd/pkg/list.go
+++ cmd/pkg/list.go
@@ -11,16 +11,15 @@ import (
 func listPackages(cmd *cobra.Command, args []string) error {
 	pkgsDir := pkg.GetPackagesDir()
 
-	fmt.Println("Installed packages:")
-
 	entries, err := os.ReadDir(pkgsDir)
 	if err != nil {
 		return fmt.Errorf("reading packages directory: %v\n", err)
 	}
 
 	for _, entry := range entries {
-		fmt.Println("    - " + entry.Name())
+		fmt.Println(entry.Name())
 	}
+
 	return nil
 }
 
@@ -28,7 +27,7 @@ func newPkgListCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "list",
 		Aliases: []string{"ls"},
-		Short:   "List locally installed packages",
+		Short:   "List installed packages",
 		RunE:    listPackages,
 	}
 }
