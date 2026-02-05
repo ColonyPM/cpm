d6a7076ce75314c4d8257a31e1c7655ed2539fa2
Lukaserikss0n <lukas.emil.eriksson@outlook.com>
2026-02-05 13:57:41 +0100

    update: Refactor function.go remove follow

--- cmd/deploy/function.go
+++ cmd/deploy/function.go
@@ -1,52 +1,11 @@
 package deploycmd
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/ColonyPM/cpm/internal/pkg"
 	"github.com/ColonyPM/cpm/internal/storectx"
-	"github.com/colonyos/colonies/pkg/client"
-	"github.com/colonyos/colonies/pkg/core"
 	"github.com/spf13/cobra"
 )
 
-var Follow bool
-
-// TODO: Attribute colonies cli
-func follow(client *client.ColoniesClient, env map[string]string, process *core.Process) error {
-	var lastTimestamp int64
-	lastTimestamp = 0
-	Count := 1 // What even is this argument?
-	for {
-		logs, err := client.GetLogsByProcessSince(env["COLONIES_COLONY_NAME"], process.ID, Count, lastTimestamp, env["COLONIES_PRVKEY"])
-		if err != nil {
-			return err
-		}
-
-		process, err := client.GetProcess(process.ID, env["COLONIES_PRVKEY"])
-		if err != nil {
-			return err
-		}
-
-		if len(logs) == 0 {
-			time.Sleep(500 * time.Millisecond)
-			if process.State == core.SUCCESS {
-				fmt.Println("Process finished successfully")
-				return nil
-			}
-			if process.State == core.FAILED {
-				return fmt.Errorf("Process failed")
-			}
-			continue
-		} else {
-			for _, log := range logs {
-				fmt.Print(log.Message)
-			}
-			lastTimestamp = logs[len(logs)-1].Timestamp
-		}
-	}
-}
 
 func deployFunction(cmd *cobra.Command, args []string) error {
 	ctx := cmd.Root().Context()
@@ -58,19 +17,9 @@ func deployFunction(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	proc, err := cc.Submit(fnSpec, cfg.Colonies.Prvkey)
-	if err != nil {
+	if 	_, err := cc.Submit(fnSpec, cfg.Colonies.Prvkey); err != nil {
 		return err
 	}
-
-	if Follow {
-		env := map[string]string{
-			"COLONIES_COLONY_NAME": cfg.Colonies.ColonyName,
-			"COLONIES_PRVKEY":      cfg.Colonies.Prvkey,
-		}
-		follow(cc, env, proc)
-	}
-
 	return nil
 }
 
@@ -82,8 +31,6 @@ func newDeployFunctionCmd() *cobra.Command {
 		Args:    cobra.ExactArgs(2),
 		RunE:    deployFunction,
 	}
-
-	cmd.Flags().BoolVar(&Follow, "follow", false, "follow process logs")
 
 	return cmd
 }
