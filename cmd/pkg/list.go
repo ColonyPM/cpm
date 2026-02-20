5b89db17585bf81c24eb35b4236b931ec585732b
Lukaserikss0n <lukas.emil.eriksson@outlook.com>
2026-02-20 10:46:16 +0100

    Update: list shows all version of a downloaded package

--- cmd/pkg/list.go
+++ cmd/pkg/list.go
@@ -3,6 +3,7 @@ package pkgcmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/ColonyPM/cpm/internal/pkg"
 	"github.com/spf13/cobra"
@@ -17,7 +18,33 @@ func listPackages(cmd *cobra.Command, args []string) error {
 	}
 
 	for _, entry := range entries {
-		fmt.Println(entry.Name())
+		if !entry.IsDir() {
+			continue
+		}
+		pkgName := entry.Name()
+		pkgPath := filepath.Join(pkgsDir, pkgName)
+
+		versionEntry, err := os.ReadDir(pkgPath)
+		if err != nil {
+			return fmt.Errorf("reading versions for %s: %v", pkgName, err)
+		}
+
+		var versions []string
+		for _, version := range versionEntry {
+			if version.IsDir() {
+				versions = append(versions, version.Name())
+			}
+		}
+
+		fmt.Println(pkgName)
+		for i, version := range versions {
+			branch := "├─"
+			if i == len(versions)-1 {
+				branch = "└─"
+			}
+			fmt.Printf("  %s %s\n", branch, version)
+		}
+
 	}
 
 	return nil
