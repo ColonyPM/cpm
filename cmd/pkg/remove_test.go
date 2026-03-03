44e4a35f1be19678a2ccfe95a2759be2ad5c82da
david1br@gmail.com <david01br@gmail.com>
2026-03-03 12:29:25 +0100

    Bugfix: Working mocking, otherwise wip

--- cmd/pkg/remove_test.go
+++ cmd/pkg/remove_test.go
@@ -14,24 +14,30 @@ import (
 // 2. Package properly removed.
 
 
-func testremovecmd(t *testing.T) {
+func TestRemoveCmd(t *testing.T) {
 	//refer to Install_test.go
 	ctx := context.Background()
 	cmd := &cobra.Command{}
 	cmd.SetContext(ctx)
 
-	//1. Check handling for incorrect path
 	tempdir := t.TempDir()
-	require.NoError(t, os.MkdirAll(filepath.Join(tempdir,"testpkg"),0o644))
+	GetPackages = func() string { return tempdir }
+	testpkg := "testpkg"
+
+	//1. Check handling for incorrect path
+	//tempdir := t.TempDir()
+	//pkgDir := tempdir
+	require.NoError(t, os.MkdirAll(filepath.Join(tempdir,testpkg),0o755))
 	require.NoError(t,os.WriteFile(filepath.Join(tempdir, "testpkg/Testfile.yaml"),[]byte("Test"),0o644))
 	
 	err := RemovePkg(cmd,[]string{"Fake"})
-	assert.EqualError(t, err,"No package found.: %w")
-	assert.NotEmpty(t,filepath.Join(tempdir,"testpkg")) 
+	assert.Error(t, err)
+	assert.NotEmpty(t,filepath.Join(tempdir,testpkg)) 
 	
 	//2. Is package removed?
-	temptest := filepath.Join(tempdir,"testpkg")
-	RemovePkg(cmd,[]string{temptest})
-	require.Empty(t,tempdir)
+	//temptest := filepath.Join(tempdir,testpkg)
+	err = RemovePkg(cmd,[]string{testpkg})
+	require.NoError(t,err)
+	require.Empty(t,testpkg)
 
 }
